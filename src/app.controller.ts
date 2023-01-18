2ff1ca408e4daf39889dda2a6ac7be359a7913e5
wlLima <lima14.wl@gmail.com>
2023-01-17 23:56:21 -0300

    Test Route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('world')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('key')
+  postTeste(@Req() request){
+    console.log(request.body)
+    return 'OK'
   }
 }
