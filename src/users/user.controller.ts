8b1bae619626f8975852ec34ef46b8965e78af3c
wlLima <lima14.wl@gmail.com>
2023-03-20 20:43:18 -0300

    feat: add user module and interfaces

---
+++ src/users/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('list')
+  async list() {
+    return this.userService.list();
+  }
+
+}
