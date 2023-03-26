b4fddb00a082958beabc3ea447f2ebbd85737515
wlLima <lima14.wl@gmail.com>
2023-03-25 22:48:34 -0300

    Fix: change imports and exports for user and auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import {AuthController} from './auth.controller'
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { User } from '../users/user.entity'
+import { UserModule } from 'src/users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
