c7a2176513919e9e151aab806456f68a6071cea5
wlLima <lima14.wl@gmail.com>
2023-06-11 13:11:55 -0300

    feat: updated status code on routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Request, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Body,
+  UseGuards,
+  HttpCode,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
@@ -8,20 +16,22 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @HttpCode(201)
   async Register(@Body() data) {
     return this.authService.register(data);
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @HttpCode(200)
   async login(@Request() req) {
-    return this.authService.login(req.user)
+    return this.authService.login(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('/profile')
-  async profile(@Request() req){
-    return await this.authService.profile(req.body.email)
+  @HttpCode(200)
+  async profile(@Request() req) {
+    return await this.authService.profile(req.body.email);
   }
-
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/users/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from './constants'
+import { jwtConstants } from './constants';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
-    private jwtService: JwtService
-  ){}
+    private jwtService: JwtService,
+  ) {}
 
   register(data) {
-    this.userService.create(data)
+    this.userService.create(data);
   }
 
-  async profile(data){
-    const teste = await this.userService.findOne(data)
-    return teste
+  async profile(data) {
+    const teste = await this.userService.findOne(data);
+    return teste;
   }
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
     if (user && user.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
@@ -32,7 +33,7 @@ export class AuthService {
     const payload = { username: user.name, sub: user.id };
 
     return {
-      access_token: this.jwtService.sign(payload, {secret: jwtConstants()}),
+      access_token: this.jwtService.sign(payload, { secret: jwtConstants() }),
     };
   }
 }
